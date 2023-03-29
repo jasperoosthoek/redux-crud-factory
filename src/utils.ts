49f64550c1d0d0ae98764950d9f69e1d8ac366c5
Jasper Oosthoek <jaspreoosthoek@hotmail.com>
2023-03-30 01:21:33 +0200

    #27 Convert utils.js to TypeScript

--- src/utils.ts
+++ src/utils.ts
@@ -1,25 +1,25 @@
 
-export const snakeToCamelCase = str => str.toLowerCase().replace(
+export const snakeToCamelCase = (str: string) => str.toLowerCase().replace(
   /([-_][a-z])/g,
   (group) => group.toUpperCase()
     .replace('-', '')
     .replace('_', '')
 );
 
-export const camelToSnakeCase = str => (str
+export const camelToSnakeCase = (str: string) => (str
   .split(/(?=[A-Z])/)
   .map(x => x.toUpperCase())
   .join('_')
 );
 
-export const toUpperCamelCase = str => {
+export const toUpperCamelCase = (str: string) => {
   // First make str camelCase in case it has a leading _
   const camelCaseId = snakeToCamelCase(str);
   // Now return "Id"
   return `${camelCaseId.charAt(0).toUpperCase()}${camelCaseId.slice(1)}`;
 }
 
-export const pluralToSingle = str => {
+export const pluralToSingle = (str: string) => {
   if (str.slice(-1).toLowerCase() !== 's') {
     // This string is not plural: keep it unaltered
     return str;
@@ -35,7 +35,7 @@ export const pluralToSingle = str => {
   } 
 }
 
-export const singleToPlural = str => {
+export const singleToPlural = (str: string) => {
   if (str.slice(-1).toLowerCase() === 's') {
     // This string is already plural: keep it unaltered
     return str;
@@ -51,13 +51,49 @@ export const singleToPlural = str => {
   } 
 }
 
-export const titleCase = str => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+export const titleCase = <T extends string>(str: T) =>`${str.charAt(0).toUpperCase()}${str.slice(1)}` as Capitalize<T>;
 
 // Simply call func with params if it is a function, if not ignore
-export const callIfFunc = (func, ...params) => {
+export const callIfFunc = (func: any, ...params: any[]) => {
   if (typeof func === 'function') {
     func(...params);
   }
 }
 
-export const arrayToObject = (array, byKey) => Object.fromEntries(array.map(obj => [obj[byKey], obj]));
+// From https://stackoverflow.com/a/69019874
+export type ObjectType = Record<PropertyKey, unknown>;
+
+// From https://stackoverflow.com/a/55153000
+type PickByValue<OBJ_T, VALUE_T> = Pick<OBJ_T, { [K in keyof OBJ_T]: OBJ_T[K] extends VALUE_T ? K : never }[keyof OBJ_T]>;
+
+// From https://stackoverflow.com/a/60142095
+export type ObjectEntries<OBJ_T> = { [K in keyof OBJ_T]: [keyof PickByValue<OBJ_T, OBJ_T[K]>, OBJ_T[K]] }[keyof OBJ_T][];
+
+export const objectEntries = <OBJ_T extends ObjectType>(obj: OBJ_T): ObjectEntries<OBJ_T> => {
+    return Object.entries(obj) as ObjectEntries<OBJ_T>;
+}
+
+export type EntriesType = [PropertyKey, unknown][] | ReadonlyArray<readonly [PropertyKey, unknown]>;
+
+// Existing Utils
+type DeepWritable<OBJ_T> = { -readonly [P in keyof OBJ_T]: DeepWritable<OBJ_T[P]> };
+type UnionToIntersection<UNION_T> // From https://stackoverflow.com/a/50375286
+    = (UNION_T extends any ? (k: UNION_T) => void : never) extends ((k: infer I) => void) ? I : never;
+
+// New Utils
+type UnionObjectFromArrayOfPairs<ARR_T extends EntriesType> =
+    DeepWritable<ARR_T> extends (infer R)[] ? R extends [infer key, infer val] ? { [prop in key & PropertyKey]: val } : never : never;
+type MergeIntersectingObjects<ObjT> = {[key in keyof ObjT]: ObjT[key]};
+export type EntriesToObject<ARR_T extends EntriesType> = MergeIntersectingObjects<UnionToIntersection<UnionObjectFromArrayOfPairs<ARR_T>>>;
+
+export const objectFromEntries = <ARR_T extends EntriesType>(arr: ARR_T): EntriesToObject<ARR_T> => {
+    return Object.fromEntries(arr) as EntriesToObject<ARR_T>;
+}
+
+const arrayMap = <T extends any>(array: T[], byKey: PropertyKey ) =>
+  array.map((obj: T) => [obj[byKey], obj]) as [any, T][];
+
+export const arrayToObject = <
+  T extends any,
+  K extends keyof T,
+>(array: T[], byKey: K) => objectFromEntries(arrayMap(array, byKey)) as {[key: PropertyKey]: T};
